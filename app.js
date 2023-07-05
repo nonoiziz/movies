112d639a7130480a2d2905b91520057567214f2e
Matthew <minhsiutsai@gmail.com>
2023-07-05 11:21:53 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+const movies = require('./public/jsons/movies.json').results
+const BASE_IMG_URL = 'https://movie-list.alphacamp.io/posters/'
 
 app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
@@ -13,12 +15,13 @@ app.get('/', (req, res) => {
 })
 
 app.get('/movies', (req, res) => {
-  res.render('index')
+  res.render('index', { movies, BASE_IMG_URL })
 })
 
 app.get('/movie/:id', (req, res) => {
   const id = req.params.id
-  res.send(`read movie: ${id}`)
+  const movie = movies.find((mv) => mv.id.toString() === id)
+  res.render(`detail`, {movie, BASE_IMG_URL})
 })
 
 app.listen(port, () => {
